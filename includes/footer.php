e8a5be3de4d53b1a19c6e98caf1ec79a3eb02002
Sylvain Duhau-marmon <sdmarmon@ensc.fr>
2017-03-13 17:32:45 +0100

    Modification footer Signed-off-by: Sylvain Duhau-marmon <sdmarmon@ensc.fr>

--- includes/footer.php
+++ includes/footer.php
@@ -7,7 +7,7 @@
                 <a href="http://www.adcog.fr/contact">Contact</a>
             </div>
             <div class="pull-right">
-                <i class="fa fa-facebook-square" style="font-size:24px;color:	#3b5998"></i><a href="https://www.facebook.com/groups/adcog/">Facebook</a>
+                <i class="fa fa-facebook-square" style="font-size:24px;color:#3b5998"></i><a href="https://www.facebook.com/groups/adcog/">Facebook</a>
                 <i class="fa fa-twitter-square" style="font-size:24px;color:#00aced"></i><a href="https://twitter.com/AssoAdcog/">Twitter</a>
                 <i class="fa fa-youtube-play" style="font-size:24px;color:red"></i><a href="https://www.youtube.com/user/AdCogOfficial">YouTube</a>
             </div>
