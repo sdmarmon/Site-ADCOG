c32dff2a733445eb36b74b515a8a1cea52b9d6de
sdmarmon <sdmarmon@ensc.fr>
2017-03-24 11:24:48 +0100

    Update footer Signed-off-by: sdmarmon <sdmarmon@ensc.fr>

--- includes/footer.php
+++ includes/footer.php
@@ -2,13 +2,13 @@
     <div class="container">
         <div class="pull-left">
             <a href="index.php">Accueil</a> | 
-            <a href="https://ensc.bordeaux-inp.fr/fr">ENSC</a> |
-            <a href="http://www.adcog.fr/contact">Contact</a>
+            <a href="https://ensc.bordeaux-inp.fr/fr" target="_blank">ENSC</a> |
+            <a href="http://www.adcog.fr/contact" target="_blank">Contact</a>
         </div>
         <div class="pull-right">
-            <i class="fa fa-facebook-square" style="font-size:18px;color:#3b5998"></i><a href="https://www.facebook.com/groups/adcog/"> Facebook </a>
-            <i class="fa fa-twitter-square" style="font-size:18px;color:#00aced"></i><a href="https://twitter.com/AssoAdcog/"> Twitter </a>
-            <i class="fa fa-youtube-play" style="font-size:18px;color:red"></i><a href="https://www.youtube.com/user/AdCogOfficial"> YouTube </a>
+            <i class="fa fa-facebook-square" style="font-size:18px;color:#3b5998"></i><a href="https://www.facebook.com/groups/adcog/" target="_blank"> Facebook </a>
+            <i class="fa fa-twitter-square" style="font-size:18px;color:#00aced"></i><a href="https://twitter.com/AssoAdcog/" target="_blank"> Twitter </a>
+            <i class="fa fa-youtube-play" style="font-size:18px;color:red"></i><a href="https://www.youtube.com/user/AdCogOfficial" target="_blank"> YouTube </a>
         </div>
     </div>
 </footer>
