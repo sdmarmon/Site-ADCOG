15ce144c7fe2bbf7f85e9b3cc62dacc7332a855d
Valentin Mancier <vmancier@ensc.fr>
2017-03-28 12:35:59 +0200

    adcog logo Onglet
    Signed-off-by: Valentin Mancier <vmancier@ensc.fr>

--- includes/head.php
+++ includes/head.php
@@ -2,6 +2,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/jpg" sizes="256x256" href="images/adcog_logo.jpg" />
     <link href="lib/bootstrap/css/bootstrap.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
